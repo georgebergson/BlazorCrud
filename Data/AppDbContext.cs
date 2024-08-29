b8a17654564b51de81e14f2bec7649b73525f0c8
George Bergson <george_bergson@hotmail.com>
2024-08-28 23:26:22 -0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Data
+{
+    public class AppDbContext : DbContext { 
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
